7aecb6b5090359d71dea745c26d61384874e553b
zhangyu <zhangyu@youint.com>
2022-05-09 00:00:16 +0800

    test: 测试commit自动修复

--- src/store/modules/app/index.js
+++ src/store/modules/app/index.js
@@ -1,8 +1,8 @@
-import { defineStore } from 'pinia';
-import defaultSettings from '@/config/settings.json';
+import { defineStore } from 'pinia'
+import defaultSettings from '@/config/settings.json'
 // 对项目layout的一些配置
 const useAppStore = defineStore('app', {
   state: () => ({ ...defaultSettings }),
   getters: {},
-});
-export default useAppStore;
+})
+export default useAppStore
